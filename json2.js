a98472166fa71a33fde58d737aa9cd5fcb83236d
nistha212002 <nisthasinghindore@gmail.com>
2024-11-06 14:52:08 +0530

    final commit

--- json2.js
+++
@@ -1,9 +0,0 @@
-import json2html from './json2html.js';
-
-const data = [
-    { Name: "Alice", Age: 25 },
-    { Name: "Bob", Age: 30 },
-    { Name: "Charlie", Age: 35, Gender: "M" }
-];
-
-document.body.innerHTML = json2html(data);
